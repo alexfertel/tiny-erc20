f01be116f6929264c6334750ef6e8abef9c33f08
Alexander Gonzalez <alexfertel97@gmail.com>
2022-10-31 11:34:49 +0200

    ref: use contract creation values

--- test/Tiny.t.sol
+++ test/Tiny.t.sol
@@ -8,7 +8,7 @@ contract TinyTest is Test {
     Tiny public tiny;
 
     function setUp() public {
-        tiny = new Tiny("Tiny", "TNY", 18, 1000000000000000000000000);
+        tiny = new Tiny("TinyUSD", "TNY", 18, 100000000000000000000000);
     }
 
     function testHi() public {
